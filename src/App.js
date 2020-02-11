30e4803e214254c45bee5fa0ca1c69c75379da93
Sean <seanmcleod93@gmail.com>
2020-02-11 20:38:45 +1100

    Added Card components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+
+import { CardList } from "./components/card-list/card-list.component";
+
+import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      string: 'Hello Sean McLeod'
-    }
+      monsters: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            { this.state.string }
-          </p>
-          <button onClick={() => this.setState({ string:'Goodbye Sean McLeod'})}>Change Text</button>
-        </header>
+        <CardList monsters={this.state.monsters} />
       </div>
     );
   }
